bee67b8232aaf9afba65789407efd5aa74992c6d
Your Name <you@example.com>
2019-01-31 18:31:07 +0530

    change in code

--- sales_invoice_tool/sales_invoice_tool/doctype/sales_invoice_tool/temp.py
+++ sales_invoice_tool/sales_invoice_tool/doctype/sales_invoice_tool/temp.py
@@ -11,57 +11,24 @@ from frappe.utils import money_in_words
 
 class SalesInvoiceTool(Document):
 	def validate(self):
-		control_amount = frappe.get_value('Invoice Tool Setting', 'ITS/001', 'control_amount')
 		items = []
-		amount = 0
-		last_qty = -1
 		for d in self.sales_invoice_tool_item:
-			for i in range(d.qty):
-				amount = amount + d.rate
-				if(amount > control_amount):
-					amount = amount-d.rate
-					if(last_qty>0):
-						items.append({"item_code": d.item_code,"qty": i-last_qty,"rate": d.rate,"warehouse":d.warehouse})
-					else:
-						items.append({"item_code": d.item_code,"qty": i,"rate": d.rate,"warehouse":d.warehouse})
-					last_qty = i;
-					sales_invoice = frappe.get_doc({
-					"doctype": "Sales Invoice", 
-					"customer": self.customer_name, 
-					"posting_date": self.posting_date,
-					"base_discount_amount":self.additional_discount_amount,
-					"additional_discount_percentage":self.additional_discount_percentage,
-					"apply_discount_on":"Net Total",
-					"total":amount,
-					"taxes_and_charges":self.taxes,
-					"created_from":self.name,
-					"items": items
-					})
-					sales_invoice.insert(ignore_permissions=True)
-					sales_invoice.save()
-					amount = 0
-					items = []
-			if(last_qty>0):
-				items.append({"item_code": d.item_code,"qty": d.qty-last_qty,"rate": d.rate,"warehouse":d.warehouse})
-			else:
-				items.append({"item_code": d.item_code,"qty": d.qty,"rate": d.rate,"warehouse":d.warehouse})
-			last_qty = -1
-
-		if(amount > 0):
-			sales_invoice = frappe.get_doc({
-			"doctype": "Sales Invoice", 
-			"customer": self.customer_name, 
-			"posting_date": self.posting_date,
-			"base_discount_amount":self.additional_discount_amount,
-			"additional_discount_percentage":self.additional_discount_percentage,
-			"apply_discount_on":"Net Total",
-			"total":amount,
-			"taxes_and_charges":self.taxes,
-			"created_from":self.name,
-			"items": items
-			})
-			sales_invoice.insert(ignore_permissions=True)
-			sales_invoice.save()
+			item_li = {"item_code": d.item_code,"qty": d.qty,"rate": d.rate,"warehouse":d.warehouse}
+			items.append(item_li)
+		sales_invoice = frappe.get_doc({
+		"doctype": "Sales Invoice", 
+		"customer": self.customer_name, 
+		"posting_date": self.posting_date,
+		"base_discount_amount":self.additional_discount_amount,
+		"additional_discount_percentage":self.additional_discount_percentage,
+		"apply_discount_on":"Net Total",
+		"total":self.total,
+		"taxes_and_charges":self.taxes,
+		"created_from":self.name,
+		"items": items
+		})
+		sales_invoice.insert(ignore_permissions=True)
+		sales_invoice.save()
 
 @frappe.whitelist(allow_guest=True)
 def getStockBalance(item_code, warehouse):
